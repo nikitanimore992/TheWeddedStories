002c69fa52e880e11f8956211d78c4486ccc89a9
nikita nimore <992nikitanimore@gmail.com>
2024-08-07 02:46:24 +0530

    creating navbar

--- Componets/Layout.jsx
+++ Componets/Layout.jsx
@@ -2,22 +2,24 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Outlet  } from 'react-router-dom';
+
 const Layout=()=>{
     return(
         <>
-            <Navbar class="text-white bg-dark">
-                    {/* <Navbar.Brand href="/home">Navbar</Navbar.Brand>  */}
-                    <Nav className="me-auto">
-                        <Nav.Link href="/home">Home</Nav.Link> 
-                        <Nav.Link href="/contactus">ContactUs</Nav.Link> 
-                    </Nav>
-            </Navbar>
+ <Navbar fill="tabs" class="text-dark bg-white">
+           <Nav>
+           <Nav.Link>Home</Nav.Link>
+           <Nav.Link>About</Nav.Link>
+           <Nav.Link>About</Nav.Link>
+      
+           </Nav>
+    </Navbar>
             <hr size="2" />
-            <Navbar class="text-white bg-dark" >
+            <Navbar  class="text-white bg-dark">
                     {/* <Navbar.Brand href="/home">Navbar</Navbar.Brand>  */}
                     <Nav className="me-auto">
-                        <Nav.Link href="/home">Home</Nav.Link> 
-                        <Nav.Link href="/contactus">ContactUs</Nav.Link> 
+                        <Nav.Link href="/home"><b>Home</b></Nav.Link> 
+                        <Nav.Link href="/contactus"><b>ContactUs</b></Nav.Link> 
                     </Nav>
             </Navbar>
             <hr size="4" color="red" />
