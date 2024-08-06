efb8a6b4029e24854b9522daaa2372395dd8d984
nikita nimore <992nikitanimore@gmail.com>
2024-08-07 03:13:33 +0530

    second nabvar

--- Componets/Layout.jsx
+++ Componets/Layout.jsx
@@ -1,31 +1,80 @@
-
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Outlet  } from 'react-router-dom';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Outlet } from 'react-router-dom';
 
-const Layout=()=>{
-    return(
+const Layout = () => {
+    return (
         <>
- <Navbar fill="tabs" class="text-dark bg-white">
-           <Nav>
-           <Nav.Link>Home</Nav.Link>
-           <Nav.Link>About</Nav.Link>
-           <Nav.Link>About</Nav.Link>
-      
-           </Nav>
-    </Navbar>
-            <hr size="2" />
-            <Navbar  class="text-white bg-dark">
-                    {/* <Navbar.Brand href="/home">Navbar</Navbar.Brand>  */}
-                    <Nav className="me-auto">
-                        <Nav.Link href="/home"><b>Home</b></Nav.Link> 
-                        <Nav.Link href="/contactus"><b>ContactUs</b></Nav.Link> 
-                    </Nav>
+            <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+                <Container>
+
+                    {/* <Navbar.Brand href="/home">StoriesWedded</Navbar.Brand> */}
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/home">Home</Nav.Link>
+                            <Nav.Link href="/photographer">Photographer</Nav.Link>
+                            <Nav.Link href="/contact">Contact</Nav.Link>
+                            {/* <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+    </NavDropdown>*/}
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">+91 975 542 6241</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                pramodnimore143@gmail.com
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Container>
             </Navbar>
+
+
+            {/* second navbar */}
+
+            <Navbar expand="lg" className="bg-body-tertiary justify-content-center">
+                <Container>
+                    <Navbar.Brand href="#home">The<b>Wedded</b>Stories</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            {/* layout */}
+
+
+
             <hr size="4" color="red" />
             <Outlet />
             <hr size="4" color="red" />
-           
+
         </>
     )
 }
