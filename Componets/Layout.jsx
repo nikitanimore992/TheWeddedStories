926e2604f11d80b8bf436e158f604789add6ab43
nikita nimore <992nikitanimore@gmail.com>
2024-08-11 21:51:29 +0530

    add nav

--- Componets/Layout.jsx
+++ Componets/Layout.jsx
@@ -17,17 +17,7 @@ const Layout = () => {
                             <Nav.Link href="/home">Home</Nav.Link>
                             <Nav.Link href="/photographer">Photographer</Nav.Link>
                             <Nav.Link href="/contact">Contact</Nav.Link>
-                            {/* <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-    </NavDropdown>*/}
+                           
                         </Nav>
                         <Nav>
                             <Nav.Link href="#deets">+91 975 542 6241</Nav.Link>
@@ -43,7 +33,7 @@ const Layout = () => {
 
             {/* second navbar */}
 
-            <Navbar expand="lg" className="bg-body-tertiary justify-content-center">
+            <Navbar expand="lg" className="bg-body-tertiary justify-content-end">
                 <Container>
                     <Navbar.Brand href="#home">The<b>Wedded</b>Stories</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
