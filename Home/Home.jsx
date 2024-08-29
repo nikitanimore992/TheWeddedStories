5cf686d5b5e76286d3fbb72af4ea3d228389c5be
nikita nimore <992nikitanimore@gmail.com>
2024-08-29 17:20:42 +0530

    update home page

--- Home/Home.jsx
+++ Home/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
         <>
             <center class="homeid">
                 <div>
-                    <img class="him1" src="images/bi.jpg" alt="" />
-                    <img class="him1" src="images/b2.jpg" alt="" />
+                    {/* <img class="himg" src="images/bi.jpg" alt="" /> */}
+                    <img class="himg" src="images/b2.jpg" alt="" />
                     <img class="himg" src="images/b3.jpg" alt="" />
                     <img class="himg" src="images/b4.jpg" alt="" />
                     <img class="himg" src="images/imgaes.jpg" alt="" />
@@ -15,6 +15,10 @@ const Home = () => {
                     <img class="himg1" src="images/bridge.jpeg" alt="" />
                     <img class="himg2" src="images/images111.jpg" alt="" />
                     
+                </div>
+                <div>
+                    <img class="him1" src="images/bi.jpg" alt="" />
+                    <img class="himg1" src="images/heand.jpeg" alt="" />
                 </div>
 
             </center>
