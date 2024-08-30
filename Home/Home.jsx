78cd1069b3630cca0db5d4fa3ca7324e0df93441
nikita nimore <992nikitanimore@gmail.com>
2024-08-30 22:56:20 +0530

    add home page contant

--- Home/Home.jsx
+++ Home/Home.jsx
@@ -2,15 +2,26 @@
 const Home = () => {
     return (
         <>
+
+            <center>
+                <div>
+                    <br />
+                    <br />
+                    <h3>
+                        FINE ART NATURE & LANDSCAPE PHOTOGRAPHER GALLERIES.
+                    </h3>
+                    <br />
+                    <p class="pere">These curated galleries feature my fine art landscape & nature photography from travels over the past twenty years. I specialize in creating beautiful images of the natural environment including mountains, wilderness areas and gardens. My adventures have taken me to many places including California, Utah, Colorado and Italy. I offer limited and open edition photography prints on ready-to-hang TruLife® Acrylic face mounts and ChromaLuxe Edge Mounts. Canson Infinity Platine Fibre watercolor prints are also available if you wish to work with your own framer.</p>
+                    <br />
+                    <p class="pere">These curated galleries feature my fine art landscape & nature photography from travels over the past twenty years. I specialize in creating beautiful images of the natural environment including mountains, wilderness areas and gardens. My adventures have taken me to many places including California, Utah, Colorado and Italy. I offer limited and open edition photography prints on ready-to-hang TruLife® Acrylic face mounts and ChromaLuxe Edge Mounts. Canson Infinity Platine Fibre watercolor prints are also available if you wish to work with your own framer.</p>
+<br />
+<br />
+                </div>
+            </center>
             <center class="homeid">
                 <div>
-                    <h1>
-                        
-                    </h1>
-                </div>
-                <div>
                     {/* <img class="himg" src="images/bi.jpg" alt="" /> */}
-                  
+
                     <img class="himg" src="images/b2.jpg" alt="" />
                     <img class="himg" src="images/b3.jpg" alt="" />
                     <img class="himg" src="images/b4.jpg" alt="" />
@@ -20,10 +31,10 @@ const Home = () => {
                 <div>
                     <img class="himg1" src="images/bridge.jpeg" alt="" />
                     <img class="himg2" src="images/images111.jpg" alt="" />
-                    
+
                 </div>
                 <div>
-                   
+
                     <img class="himg3" src="images/heand.jpeg" alt="" />
                     <img class="himg4" src="images/bi.jpg" alt="" />
                     <img class="himg4" src="images/imgaes.jpg" alt="" />
